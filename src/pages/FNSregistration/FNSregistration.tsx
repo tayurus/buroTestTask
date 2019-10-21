dcf559927070a5282e0ae6cdc6bf40e529d78dea
tayurus <tayurus@gmail.com>
2019-10-21 13:51:00 +0300

    add tabs component from antd

--- src/pages/FNSregistration/FNSregistration.tsx
+++ src/pages/FNSregistration/FNSregistration.tsx
@@ -2,30 +2,48 @@ import React, { Component } from "react";
 import { withNaming } from "@bem-react/classname";
 import "./FNSregistration.scss";
 
+import { Tabs } from "antd";
 import { connect } from "react-redux";
 
 import { RequestsCommonInfo } from "./RequestsCommonInfo/RequestsCommonInfo";
 
+const { TabPane } = Tabs;
+
 const cn = withNaming({ n: "", e: "__", m: "_", v: "_" });
 const b = cn("fns-registration");
+
+const commonInfo = {
+  balance: 400000,
+  total: 2,
+  processing: 6,
+  ended: 8,
+  archive: 1
+};
+
+const tabs = ["Все заявки", "Регистрация ООО", "Регистрация ИП", "Изменения в ИП", "Закрытие ИП"];
 
 class FNSregistration extends Component<any> {
   constructor(props: any) {
     super(props);
   }
 
-  render() {
-    const commonInfo = {
-      balance: 400000,
-      total: 2,
-      processing: 6,
-      ended: 8,
-      archive: 1
-    };
+  renderTabs = (tabs: Array<any>) => {
+    return (
+      <Tabs type="card">
+        {tabs.map((title: string, key: number) => (
+          <TabPane tab={title} key={key.toString()}>
+            {title}
+          </TabPane>
+        ))}
+      </Tabs>
+    );
+  };
 
+  render() {
     return (
       <div className={b()}>
         <RequestsCommonInfo {...commonInfo} />
+        {this.renderTabs(tabs)}
       </div>
     );
   }

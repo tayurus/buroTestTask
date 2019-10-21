b7a7ee2660c22867d0264518232bfa0ce03617b9
tayurus <tayurus@gmail.com>
2019-10-21 21:19:57 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.83e95b565a5af6eeb661fbd8d2b9c66f.js"
+  "/tayurus/buroTestTask/precache-manifest.7ffbefbf101796cfee2f829c6a04d0dc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/tayurus/buroTestTask/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
